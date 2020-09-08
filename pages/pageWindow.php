497bfc8fbe8ff49ecda9f629061eaa25a17e34db
Maxboo <mbourrier@booprod.com>
2020-09-08 11:04:29 +0200

    Début d'ajout des pages windows

---
+++ pages/pageWindow.php
@@ -0,0 +1,33 @@
+
+<html>
+  <body>
+  <form action ="inter/ajoutwindow.php" method ="get">
+      <div>
+          <input type="radio" id="nord" name="card_name" value="nord"  checked>
+          <label for="nord">nord</label>
+      </div>
+
+      <div>
+          <input type="radio" id="sud" name="card_name" value="sud">
+          <label for="sud">sud</label>
+      </div>
+
+      <div>
+          <input type="radio" id="est" name="card_name" value="est">
+          <label for="est">est</label>
+      </div>
+      <div>
+          <input type="radio" id="ouest" name="card_name" value="ouest">
+          <label for="ouest">ouest</label>
+      </div>
+
+      <label for="w_name">Nom de la fenêtre : </label>
+      <input type="input" id="name" name="w_name" value="Fenêtre">
+
+      <input type="submit" value="Envoyer">
+  </form>
+
+  </body>
+</html>
+
+<?php
