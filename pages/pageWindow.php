9d46f8dcb56dd1965ba50cf8f8f5e143a000dbaa
Mathias <mathias.boudou@epsi.fr>
2020-09-09 14:57:48 +0200

    css js

--- pages/pageWindow.php
+++ pages/pageWindow.php
@@ -21,36 +21,36 @@ include_once("patern/Head.php");
         <script src="../Javascript/jsf.js" charset="utf-8"></script>
         <form action="inter/ajoutwindow.html" method="get">
             <div>
-                <label class="btn btn-nord" for="Nord">Nord</label>
-                <input type="radio" id="Nord" name="card_name" value="nord">
+                <label for="Nord">Nord</label>
+                <input class="btn btn-nord" type="radio" id="Nord" name="card_name" value="nord">
             </div>
             <div>
-                <label class="btn btn-nordo" for="Nord-Ouest">Nord-Ouest</label>
-                <input type="radio" id="Nord-Ouest" name="card_name" value="nord-ouest">
+                <label for="Nord-Ouest">Nord-Ouest</label>
+                <input class="btn btn-nordo" type="radio" id="Nord-Ouest" name="card_name" value="nord-ouest">
             </div>
             <div>
-                <label class="btn btn-est" for="Est">Est</label>
-                <input type="radio" id="Est" name="card_name" value="est">
+                <label for="Est">Est</label>
+                <input class="btn btn-est" type="radio" id="Est" name="card_name" value="est">
             </div>
             <div>
-                <label class="btn btn-sud" for="Sud">Sud</label>
-                <input type="radio" id="Sud" name="card_name" value="sud">
+                <label  for="Sud">Sud</label>
+                <input class="btn btn-sud" type="radio" id="Sud" name="card_name" value="sud">
             </div>
             <div>
-                <label class="btn btn-ouest" for="Ouest">Ouest</label>
-                <input type="radio" id="Ouest" name="card_name" value="ouest">
+                <label  for="Ouest">Ouest</label>
+                <input class="btn btn-ouest" type="radio" id="Ouest" name="card_name" value="ouest">
             </div>
             <div>
-                <label class="btn btn-norde" for="Nord-Est">Nord-Est</label>
-                <input type="radio" id="Nord-Est" name="card_name" value="nord-est">
+                <label  for="Nord-Est">Nord-Est</label>
+                <input class="btn btn-norde" type="radio" id="Nord-Est" name="card_name" value="nord-est">
             </div>
             <div>
-                <label class="btn btn-sude" for="Sud-Est">Sud-Est</label>
-                <input type="radio" id="Sud-Est" name="card_name" value="sud-est">
+                <label  for="Sud-Est">Sud-Est</label>
+                <input class="btn btn-sude" type="radio" id="Sud-Est" name="card_name" value="sud-est">
             </div>
             <div>
-                <label class="btn btn-sudo" for="Sud-Ouest">Sud-Ouest</label>
-                <input type="radio" id="Sud-Ouest" name="card_name" value="sud-ouest">
+                <label  for="Sud-Ouest">Sud-Ouest</label>
+                <input class="btn btn-sudo" type="radio" id="Sud-Ouest" name="card_name" value="sud-ouest">
             </div>
             <input type="submit" name="button" class="bouton" value="Validez votre orientation">
         </form>
