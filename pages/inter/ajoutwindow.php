07285ac7b914ece6b4fca6bf5ec8e50d69231ecb
Maxboo <mbourrier@booprod.com>
2020-09-09 08:55:24 +0200

    ajoutwindow et page window fonctionnel mais pas fini - TODO : ajouter l'html

--- pages/inter/ajoutwindow.php
+++ pages/inter/ajoutwindow.php
@@ -5,10 +5,9 @@ require "../Fonction.php";
 
 
 if(isset($_POST["card_name"]) && isset($_POST["w_name"]) && isset($_POST["u_id"])){
-    //On utilise la liste Post pour ensuite crée une nouvelle fenêtre à insérer
     $bdd = getDataBase();
     $id_card = getListe($bdd,"cardinalite",array("card_name"=>$_POST["card_name"]));
-    unset($_POST["card_name"]); // On retire
+    unset($_POST["card_name"]);
     $id_card = $id_card[0];
     $id_card = $id_card->card_id;
     $_POST["card_id"] = $id_card;
