9ebf62ddd2f383adf9934dcfb7efe5391beed5b7
Galambur <gaelle.derambure@epsi.fr>
2020-09-10 16:56:05 +0200

    link le boutton

--- pages/index.php
+++ pages/index.php
@@ -6,7 +6,7 @@ require 'patern/Head.php';
  <div class="content">
         <div class="cursor" id="cursor"></div>
         <div class="title" id="title"><span>Smart Celsius</span></div>
-        <div class="button" id="button"><span>S'abonner</span></div>
+        <div class="button" id="button"><a href="https://twitter.com/CelsiusSmart"><span>S'abonner</span></a></div>
  </div>
 <script src="../Javascript/index.js" charset="utf-8"></script>
 </div>
